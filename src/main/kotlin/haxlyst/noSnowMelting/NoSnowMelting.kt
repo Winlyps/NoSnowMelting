b5cacd9b53b19d459b40d89a95e67a00244a55b3
Haxlyst <Haxlyst@gmail.com>
2025-07-03 11:15:10 +0200

    Initial commit

---
+++ src/main/kotlin/haxlyst/noSnowMelting/NoSnowMelting.kt
@@ -0,0 +1,14 @@
+package haxlyst.noSnowMelting
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoSnowMelting : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
