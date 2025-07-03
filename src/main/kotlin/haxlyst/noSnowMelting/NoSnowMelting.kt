9a8a90b248366f5236a0f540faafeb5b5d13bbd2
Haxlyst <Haxlyst@gmail.com>
2025-07-03 11:16:07 +0200

    code!

--- src/main/kotlin/haxlyst/noSnowMelting/NoSnowMelting.kt
+++ src/main/kotlin/haxlyst/noSnowMelting/NoSnowMelting.kt
@@ -1,14 +1,25 @@
 package haxlyst.noSnowMelting
 
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockFadeEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class NoSnowMelting : JavaPlugin() {
+class NoSnowMelting : JavaPlugin(), Listener {
 
     override fun onEnable() {
-        // Plugin startup logic
+        server.pluginManager.registerEvents(this, this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    fun onBlockFade(event: BlockFadeEvent) {
+        if (event.block.type == Material.SNOW) {
+            event.isCancelled = true
+        }
     }
 }
